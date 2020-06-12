b6e055fc5cf302b87c6df9a82f0333caa6b4fb49
Chris Sanchez <chrisvsanchez@gmail.com>
2020-06-12 20:06:56 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,3 +1,4 @@
+require 'pry'
 # Write your code here!
 def game_hash
   game_hash = 
@@ -56,9 +57,9 @@ def game_hash
             slam_dunks:1
             }
                   ]},
-    away:{                
-      {team_name: "Charlotte Hornets", 
-      colors:["Turquoise", "Purple"],
+    away:{
+      {team_name:"Charlotte Hornets",
+      colors:["Turquoise","Purple"],
       players:[
         {player_name: "Jeff Adrien",
           number:4,
@@ -69,18 +70,18 @@ def game_hash
           steals:2,
           blocks:7,
           slam_dunks:2
-          },
+        },
         {player_name: "Bismack Biyombo",
-          number:0,
-          shoe:16,
-          points:12,
-          rebounds:4,
-          assists:7,
-          steals:22,
-          blocks:15,
-          slam_dunks:10
-          },
-        {player_name: "DeSangna Diop",
+          number: 0,
+          shoe: 16,
+          points: 12,
+          rebounds: 4,
+          assists: 7,
+          steals: 22,
+          blocks: 15,
+          slam_dunks: 10
+        },
+        {player_name: "DeSagna Diop",
           number:2,
           shoe:14,
           points:24,
@@ -89,7 +90,7 @@ def game_hash
           steals:4,
           blocks:5,
           slam_dunks:5
-          },
+        },
         {player_name: "Ben Gordon",
           number:8,
           shoe:15,
@@ -99,9 +100,9 @@ def game_hash
           steals:1,
           blocks:1,
           slam_dunks:0
-          },
+        },
         {player_name: "Kemba Walker",
-          number: 33,
+          number:33,
           shoe:15,
           points:6,
           rebounds:12,
@@ -109,9 +110,9 @@ def game_hash
           steals:7,
           blocks:5,
           slam_dunks:12
-          }
+        }
         ]
-      }
+      }             
     }
-  }          
+  }      
 end
